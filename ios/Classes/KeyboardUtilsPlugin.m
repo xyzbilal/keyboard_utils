f0a35cabcb72a689e2e9c3dfcda419e8168da340
zhengzeqin <zeqinjie@qq.com>
2022-12-21 11:55:10 +0800

    fix: 依赖文件

--- ios/Classes/KeyboardUtilsPlugin.m
+++ ios/Classes/KeyboardUtilsPlugin.m
@@ -1,5 +1,5 @@
 #import "KeyboardUtilsPlugin.h"
-#import <keyboard_utils/keyboard_utils-Swift.h>
+#import <keyboard_utils_fork/keyboard_utils_fork-Swift.h>
 
 @implementation KeyboardUtilsPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
